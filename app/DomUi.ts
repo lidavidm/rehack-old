6391b9643cd31484491de4d57719780d52a1e070
David Li <li.davidm96@gmail.com>
2014-06-05 13:24:48 -0700

    Use Ractive.js for HTML UI

--- app/DomUi.ts
+++ app/DomUi.ts
@@ -1,3 +1,5 @@
+declare var Ractive: any;
+
 module NightfallHack {
     export interface UiObjectCommand {
         name: string;
@@ -8,50 +10,52 @@ module NightfallHack {
     export interface UiObject {
         title: string;
         commands: UiObjectCommand[];
+        health?: number;
+        maxHealth?: number;
+        moves?: number;
+        maxMoves?: number;
     }
     
     export class DomUi {
         private _element: HTMLElement;
-        private _title: HTMLElement;
-        private _commandList: HTMLElement;
         private _selected: UiObject;
         private _commands: UiObjectCommand[];
+        private _ractive: any;
 
         public onCommandSelected = new Phaser.Signal();
         
         constructor(element) {
             this._element = element;
-            this._title = element.querySelector('h1');
-            this._commandList = element.querySelector('#ui-commands ul');
+            this._ractive = new Ractive({
+                el: element,
+                template: '#domUi',
+                data: {}
+            });
+
+            this._ractive.on('command', (e) => {
+                console.log(e.context);
+                if (typeof e.context.handler !== "undefined") {
+                    e.context.handler();
+                }
+            });
         }
 
         objectSelected(properties: UiObject) {
             this._selected = properties;
-            this._title.innerHTML = properties.title;
-            this.menu(properties.commands);
+            this._ractive.set(properties);
         }
 
         objectDeselected() {
             this._selected = null;
-            this._title.innerHTML = '';
-            this._commandList.innerHTML = '';
+            this._ractive.set({
+                title: '',
+                commands: [],
+            });
         }
 
         menu(commands: UiObjectCommand[]) {
             this._commands = commands;
-            this._commandList.innerHTML = '';
-            for (var i = 0; i < commands.length; i++) {
-                this._commandList.innerHTML += "<li><button>" + commands[i].name + "</button></li>";
-            }
-
-            var commandNodes = this._commandList.querySelectorAll('button');
-            for (var i = 0; i < commandNodes.length; i++) {
-                ((i) => {
-                    commandNodes[i].addEventListener('click', (event) => {
-                        this._commands[i].handler();
-                    });
-                })(i);
-            }
+            this._ractive.set('commands', commands);
         }
     }
 }
